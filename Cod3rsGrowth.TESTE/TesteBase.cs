6bb85b82ce633dfbf34c63d65bdc6101a1194b21
anderson <anderson.marinho@inventsoftware.com.br>
2024-10-22 16:09:07 -0300

    Implementado configuração

---
+++ Cod3rsGrowth.TESTE/TesteBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Cod3rsGrowth.TESTE
+{
+    public class TesteBase
+    {
+        protected readonly IServiceCollection _service;
+        public TesteBase()
+        {
+            _service = DefinirInjecaoDeDependencia();
+        }
+
+        private IServiceCollection DefinirInjecaoDeDependencia()
+        {
+            var services = new ServiceCollection();
+
+            //Escopo destinado para o uso do addScoped dos futuros serviços, validadores e repositórios
+
+            return services;
+        }
+
+        protected T GetService<T>() => _service.BuildServiceProvider().GetRequiredService<T>();
+    }
+}
