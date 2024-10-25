874c4418ac76329712ec24a910fa37be6523594b
anderson <anderson.marinho@inventsoftware.com.br>
2024-10-25 17:49:09 -0300

    Feito implementacao do repositorio com linq2db

--- Cod3rsGrowth.INFRA/Repositorio/RepositorioCarro.cs
+++ Cod3rsGrowth.INFRA/Repositorio/RepositorioCarro.cs
@@ -1,34 +1,57 @@
 ﻿using Cod3rsGrowth.DOMINIO.Carros;
-using System;
+using LinqToDB;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Cod3rsGrowth.INFRA.Repositorio
 {
-    public class RepositorioCarro : IRepositorioCarro
+    public class RepositorioCarro : RepositorioBase, IRepositorioCarro
     {
         public void Atualizar(Carro carro)
         {
-            throw new NotImplementedException();
+            Carro().Where(x => x.Id == carro.Id)
+                .Set(x => x.Marca, carro.Marca)
+                .Set(x => x.Modelo, carro.Modelo)
+                .Set(x => x.AnoModelo, carro.AnoModelo)
+                .Set(x => x.AnoFabricacao, carro.AnoFabricacao)
+                .Set(x => x.Combustivel, carro.Combustivel)
+                .Set(x => x.ValorCusto, carro.ValorCusto)
+                .Set(x => x.ValorOfertado, carro.ValorOfertado)
+                .Set(x => x.ValorVenda, carro.ValorVenda)
+                .Set(x => x.ProprietarioNome, carro.ProprietarioNome)
+                .Update();
         }
 
         public void Criar(Carro carro)
         {
-            throw new NotImplementedException();
+            Conexao().Insert(carro);
         }
 
         public Carro ObterPorId(int id)
         {
-            throw new NotImplementedException();
+            return Carro().FirstOrDefault(x => x.Id == id);
         }
 
         public List<Carro> ObterTodos(Filtro filtro)
         {
-            throw new NotImplementedException();
+            var query = Carro();
+
+            if (!string.IsNullOrWhiteSpace(filtro.Modelo))
+            {
+                query = query.Where(x => x.Modelo.Equals(filtro.Modelo)) as ITable<Carro>;
+            }
+
+            return query.ToList();
         }
 
         public void Remover(int id)
         {
-            throw new NotImplementedException();
+            Carro().Where(x => x.Id == id).Delete();
+        }
+
+        private ITable<Carro> Carro()
+        {
+            return Conexao().GetTable<Carro>();
         }
     }
 }
