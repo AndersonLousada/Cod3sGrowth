b5d242bfd80c85810373abba425e1c0de5604ef9
anderson <anderson.marinho@inventsoftware.com.br>
2025-03-26 19:18:29 -0300

    Feito implementacao da tela de listagem

---
+++ Cod3rsGrowth.WEB/Controllers/AuxiliaresController.cs
@@ -0,0 +1,19 @@
+﻿using Cod3rsGrowth.DOMINIO.Carros;
+using Cod3rsGrowth.DOMINIO.Extencoes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cod3rsGrowth.WEB.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuxiliaresController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult ObterEnumeradores()
+        {
+            var enumeradores = ExtensaoEnum.GetEnumDescriptions<Combustivel>();
+            return Ok(enumeradores);
+        }
+    }
+}
